39ba0918e6b41a7843948623a9e5c87c4a19ea9d
Pongpat <pause.pongpat@gmail.com>
2022-05-29 23:02:22 +0700

    W1-02 Components Part1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from '/Users/pongpatosathanon/Documents/UNIVERSITY W/CSC105 WEB APP/Main-React-PT/confusion/src/components /MenuComponent.js'; 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
+      <Menu></Menu>
     );
   }
 }
